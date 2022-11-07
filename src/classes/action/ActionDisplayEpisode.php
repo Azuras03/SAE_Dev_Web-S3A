32e16ecc350360fd4019ed45e84737a91844fe67
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-11-07 17:17:09 +0100

    F8 InProgress adding and testing methods

--- src/classes/action/ActionDisplayEpisode.php
+++ src/classes/action/ActionDisplayEpisode.php
@@ -8,6 +8,8 @@ class ActionDisplayEpisode extends Action
     public function execute(): string
     {
         $episode = unserialize($_SESSION['episode']);
+        $user = $_SESSION["user"];
+        $user->addEpisodeInProgress();
         $renderer = new \netvod\render\RenderEpisode($episode);
         return $renderer->render();
     }
