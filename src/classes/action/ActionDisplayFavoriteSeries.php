3524f0cf0d1c8dd0cefabf13f81a85d1c0a9ab84
Azuras03 <nr03russo@gmail.com>
2022-11-08 13:48:12 +0100

    Adding the action called ActionDisplayFavoriteSeries, to display the favorite series that you have (⭐)

---
+++ src/classes/action/ActionDisplayFavoriteSeries.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace netvod\action;
+
+use netvod\db\ConnectionFactory;
+
+class ActionDisplayFavoriteSeries extends Action
+{
+
+    public function execute(): string
+    {
+        $db = ConnectionFactory::makeConnection();
+        $seriesPref = "";
+
+        $addSerie = $db->prepare("SELECT titre, img, id_serie FROM serie INNER JOIN user2serie ON user2serie.id_serie = serie.id INNER JOIN user ON user2serie.id_user = user.id WHERE user.id = ?;");
+        $addSerie->bindParam(1, unserialize($_SESSION['user'])->id);
+        if ($addSerie->execute()) {
+            while ($donnees = $addSerie->fetch()) {
+                $minia = '<img src="images/' . $donnees["img"] . '" height=200px width=500px>';
+                $url = '?action=display-serie&serie=' . $donnees["id_serie"];
+                $seriesPref .= '<a href=' . $url . '>' . $donnees['titre'] . '</a><br>'. $minia .'</br>';
+            }
+        }
+
+        return '<h4>Liste de vos séries préférées ⭐ :</h4> <p>'.$seriesPref.'</p>';
+    }
+}
