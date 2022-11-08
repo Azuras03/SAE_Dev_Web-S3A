bdbb569f995ee429a682faca4db53327a8a6f2e1
Azuras03 <nr03russo@gmail.com>
2022-11-08 10:54:38 +0100

    Adding a variable that keeps the id of the current serie ($_GET)

--- src/classes/action/Action.php
+++ src/classes/action/Action.php
@@ -7,12 +7,14 @@ abstract class Action {
     protected ?string $http_method = null;
     protected ?string $hostname = null;
     protected ?string $script_name = null;
+    protected ?string $id_serie = null;
 
     public function __construct(){
 
         $this->http_method = $_SERVER['REQUEST_METHOD'];
         $this->hostname = $_SERVER['HTTP_HOST'];
         $this->script_name = $_SERVER['SCRIPT_NAME'];
+        $this->id_serie = $_GET['id'] ?? null;
     }
 
     abstract public function execute() : string;
