3f1dde73dd7e953c2a2307de412714153c3b92d0
Azuras03 <nr03russo@gmail.com>
2022-11-08 11:07:10 +0100

    Creating the action called ActionSaveSeriePref.php, executing when someone presses the 'fav' button

---
+++ src/classes/action/ActionSaveSeriePref.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace netvod\action;
+
+use netvod\db\ConnectionFactory;
+
+class ActionSaveSeriePref extends Action
+{
+
+    public function execute(): string
+    {
+        $sql = "DELETE FROM `user2serie` WHERE id_user = ? AND id_serie = ?;";
+        $sql2 = "INSERT INTO user2serie VALUES (?, ?)";
+        $db = ConnectionFactory::makeConnection();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(1, unserialize($_SESSION['user'])->id);
+        $stmt->bindParam(2, $this->id_serie);
+        $stmt2 = $db->prepare($sql2);
+        $stmt2->bindParam(1, unserialize($_SESSION['user'])->id);
+        $stmt2->bindParam(2, $this->id_serie);
+        try{
+            $stmt->execute();
+            $stmt2->execute();
+            if($stmt->rowCount() != 0) return 'La série est déjà dans vos préférences 🟠';
+        }
+        catch (\PDOException $e){
+            return "Une erreur est survenue 🔴";
+        }
+        return "Insertion réussie 🟢";
+    }
+}
