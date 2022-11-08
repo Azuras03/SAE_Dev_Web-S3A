cfed601634a4d6589c1557c698c47fede1db111f
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-11-08 11:02:25 +0100

    F4 Create new class Serie and externalise dsplaying series list into it

--- src/classes/action/ActionDetailSerie.php
+++ src/classes/action/ActionDetailSerie.php
@@ -2,6 +2,7 @@
 
 namespace netvod\action;
 
+use netvod\catalogue\Serie;
 use netvod\db\ConnectionFactory;
 
 class ActionDetailSerie extends Action
@@ -9,18 +10,9 @@ class ActionDetailSerie extends Action
 
     public function execute(): string
     {
-        $idSerie = $_GET['id'];
+        $html = Serie::displaySerie();
+
         $db = ConnectionFactory::makeConnection();
-        $stmt = $db->prepare('SELECT titre, descriptif, annee, date_ajout FROM serie WHERE id = ?');
-        $stmt->bindParam(1, $idSerie);
-
-        if ($stmt->execute()) {
-            $donnees = $stmt->fetch();
-
-            $titre = $donnees['titre'];
-            $detail = 'Cette série de ' . $donnees['annee'] . ' a été ajouté sur netVOD le ' . $donnees['date_ajout'];
-            $desc = $donnees['descriptif'];
-        }
 
         $stmt2 = $db->prepare('SELECT numero, titre, duree FROM episode WHERE serie_id = ?');
         $stmt2->bindParam(1, $idSerie);
@@ -35,15 +27,8 @@ class ActionDetailSerie extends Action
             }
         }
 
-        return <<<HTML
-
-            <p>Détails de la série $titre :</p>
-            <p>$detail</br>
-            Descriptif : $desc</p>
-
-            <p>$episode</p>
-          
-        HTML;
+        $html .= "<p>$episode</p>";
+        return $html;
 
     }
 }
