4d3d394beae40c22af78f06bc77d942489db7503
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-11-08 11:16:45 +0100

    F4 display episode extracted in Episode class

--- src/classes/action/ActionDetailSerie.php
+++ src/classes/action/ActionDetailSerie.php
@@ -2,6 +2,7 @@
 
 namespace netvod\action;
 
+use netvod\catalogue\Episode;
 use netvod\catalogue\Serie;
 use netvod\db\ConnectionFactory;
 
@@ -11,23 +12,7 @@ class ActionDetailSerie extends Action
     public function execute(): string
     {
         $html = Serie::displaySerie();
-
-        $db = ConnectionFactory::makeConnection();
-
-        $stmt2 = $db->prepare('SELECT numero, titre, duree FROM episode WHERE serie_id = ?');
-        $stmt2->bindParam(1, $idSerie);
-        $episode = "";
-
-        if ($stmt2->execute()) {
-            while ($donneesEp = $stmt2->fetch()) {
-                $num = $donneesEp['numero'];
-                $titreEp = $donneesEp['titre'];
-                $dureeEp = $donneesEp['duree'];
-                $episode .= 'Épisode ' . $num . ' : </br> Titre : ' . $titreEp . '</br> Durée : ' . $dureeEp . ' minutes</br></br>';
-            }
-        }
-
-        $html .= "<p>$episode</p>";
+        $html .= Episode::displayEpisode();
         return $html;
 
     }
