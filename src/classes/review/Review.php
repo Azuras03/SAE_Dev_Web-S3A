8fc6f075240cdff75eb737bb8d3f3c686de462af
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-11-09 20:46:34 +0100

    update serie average note in table serie when sending a review

--- src/classes/review/Review.php
+++ src/classes/review/Review.php
@@ -7,7 +7,7 @@ use netvod\user\User;
 
 class Review
 {
-    public static function displayComments(int $serId) : string
+    public static function displayReviews(int $serId) : string
     {
         $db = ConnectionFactory::makeConnection();
         $q = "SELECT email, note, comment FROM `avis`, user
@@ -17,6 +17,7 @@ class Review
         $st->execute([$serId]);
 
         $html = "<h4>Tous les avis</h4>";
+        $init = $html;
         foreach ($st as $data) {
             $html .= <<<HTML
                     <article>
@@ -26,15 +27,16 @@ class Review
                     </article>
                     HTML;
         }
+        if ($html === $init) $html .= "Aucun avis pour l'instant...";
         return $html;
     }
 
-    public static function displayReviewForm(int $serId): string
+    public static function displayReviewForm(int $idSerie): string
     {
 
         $comment = "<h4>Ajouter un avis</h4>";
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            if (!($code = self::insertReview($_POST["commentaire"], $_POST["note"], $serId)))
+            if (!($code = self::insertReview($_POST["commentaire"], $_POST["note"], $idSerie)))
                 $comment .= "<small>La note doit être comprise entre 1 et 5 ! 🔴</small>";
             else if ($code == 1)
                 $comment .= "<small>Avis ajouté 🟢</small>";
@@ -55,7 +57,7 @@ class Review
         return $comment;
     }
 
-    public static function insertReview($commentaire, $note, $serie_id) : int|bool
+    public static function insertReview($commentaire, $note, $idSerie) : int|bool
     {
         $db = ConnectionFactory::makeConnection();
         $commentaire = filter_var($commentaire, FILTER_SANITIZE_SPECIAL_CHARS);
@@ -65,7 +67,7 @@ class Review
 
         $q = "SELECT count(*) FROM avis WHERE id_user = ? AND id_serie = ?";
         $st = $db->prepare($q);
-        $st->execute([unserialize($_SESSION['user'])->id, $serie_id]);
+        $st->execute([unserialize($_SESSION['user'])->id, $idSerie]);
         $data = $st->fetch();
 
         if ($data[0] > 0)
@@ -75,16 +77,28 @@ class Review
                 WHERE id_user = ?
                 AND id_serie = ?";
             $st = $db->prepare($q);
-            $st->execute([$note, $commentaire, unserialize($_SESSION['user'])->id, $serie_id]);
-            return 2;
+            $st->execute([$note, $commentaire, unserialize($_SESSION['user'])->id, $idSerie]);
+            $res = 2;
         }
         else
         {
             $q = "INSERT INTO avis(id_user, id_serie, note, comment) VALUES (?,?,?,?)";
             $st = $db->prepare($q);
-            $st->execute([unserialize($_SESSION['user'])->id, $serie_id, $note, $commentaire]);
-            return 1;
+            $st->execute([unserialize($_SESSION['user'])->id, $idSerie, $note, $commentaire]);
+            $res = 1;
         }
+        self::updateAvgSerieReview($idSerie);
+        return $res;
+    }
+
+    public static function updateAvgSerieReview(int $idSerie): void
+    {
+        $db = ConnectionFactory::makeConnection();
+        $q = "UPDATE serie 
+               SET serie.note_moy = (SELECT avg(note) FROM avis WHERE id_serie = ?)
+               WHERE id = ?";
+        $st = $db->prepare($q);
+        $st->execute([$idSerie, $idSerie]);
     }
 
 
