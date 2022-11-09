7fe0a2384f3d3422feb72cfcefde23490ca15206
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-11-09 19:10:51 +0100

    delete commented code

--- src/classes/action/ActionDisplayReviews.php
+++ src/classes/action/ActionDisplayReviews.php
@@ -15,32 +15,6 @@ class ActionDisplayReviews extends Action
     public function execute(): string
     {
         $idSerie = $_GET['id'];
-
         return Review::displayComments($idSerie);
-        /*
-        $db = ConnectionFactory::makeConnection();
-        $stmt = $db->prepare('SELECT * FROM episode WHERE serie_id = ? AND numero = ?');
-        $stmt->bindParam(1, $idSerie);
-        $stmt->bindParam(2, $idEpisode);
-        $episode = "";
-        if ($stmt->execute()) {
-            $donnees = $stmt->fetch();
-            $id = $donnees['id'];
-            $numero = $donnees['numero'];
-            $titre = $donnees['titre'];
-            $resume = $donnees['resume'];
-            $duree = $donnees['duree'];
-            $file = $donnees['file'];
-            $serId = $donnees['serie_id'];
-            $episode = new Episode($id, $numero, $titre, $resume, $file, $duree, $idSerie);
-        }
-        $renderer = new \netvod\render\RenderEpisode($episode);
-        $user = unserialize($_SESSION["user"]);
-        $user->addEpisodeInProgress($id);
-
-
-        $comment = Review::displayReviewForm($serId);
-        return $renderer->render() . $comment;
-        */
     }
 }
