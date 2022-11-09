07429ef128a248557150ed5696f8833486d6bd09
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-11-09 11:24:14 +0100

    Insert avis ok

--- src/classes/action/ActionDisplayEpisode.php
+++ src/classes/action/ActionDisplayEpisode.php
@@ -33,22 +33,27 @@ class ActionDisplayEpisode extends Action
         $user = unserialize($_SESSION["user"]);
         $user->addEpisodeInProgress($id);
 
-        $form = "";
-        if ($_SERVER['REQUEST_METHOD'] == 'GET')
-            $form = <<<HTML
-            <form method="post" action="">
-                <label>Commentaire : </label>
-                <textarea name="commentaire" placeholder="<Ecrivez ici>"></textarea><br>
-                <label>Note : </label>
-                <input type="number" name="note" placeholder="<note>" min="0" max="5"><br>
-                <button type="submit">Envoyer</button>
-            </form>
-            HTML;
-        else
+        $comment = "";
+        if ($_SERVER['REQUEST_METHOD'] == 'POST')
         {
-            User::insertAvis($_POST["commentaire"], $_POST["note"], $id);
+            if (!($code = User::insertAvis($_POST["commentaire"], $_POST["note"], $id)))
+                $comment .= "<small>La note doit être comprise entre 0 et 5 ! 🔴</small>";
+            else if ($code == 1)
+                $comment .= "<small>Avis ajouté 🟢</small>";
+            else if ($code == 2)
+                $comment .= "<small>Avis mis à jour 🟢</small>";
         }
 
-        return $renderer->render() . $form;
+        $comment .= <<<HTML
+        <form method="post" action="">
+            <label>Commentaire : </label>
+            <textarea name="commentaire" placeholder="<Ecrivez ici>"></textarea><br>
+            <label>Note : </label>
+            <input type="number" name="note" placeholder="<note>" min="0" max="5"><br>
+            <button type="submit">Envoyer</button>
+        </form>
+        HTML;
+
+        return $renderer->render() . $comment;
     }
 }
