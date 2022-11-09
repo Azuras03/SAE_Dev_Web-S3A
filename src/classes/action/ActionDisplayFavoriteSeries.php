0a9b3a97cbc787c7e8004bab7505375b0ce2356f
Azuras03 <nr03russo@gmail.com>
2022-11-09 17:26:55 +0100

    Adding changes to the favorite titles

--- src/classes/action/ActionDisplayFavoriteSeries.php
+++ src/classes/action/ActionDisplayFavoriteSeries.php
@@ -15,13 +15,17 @@ class ActionDisplayFavoriteSeries extends Action
         $addSerie = $db->prepare("SELECT titre, img, id_serie FROM serie INNER JOIN user2serie ON user2serie.id_serie = serie.id INNER JOIN user ON user2serie.id_user = user.id WHERE user.id = ?;");
         $addSerie->bindParam(1, unserialize($_SESSION['user'])->id);
         if ($addSerie->execute()) {
-            while ($donnees = $addSerie->fetch()) {
-                $minia = '<img src="images/' . $donnees["img"] . '" height=200px width=500px>';
-                $url = '?action=display-serie&serie=' . $donnees["id_serie"];
-                $seriesPref .= '<a href=' . $url . ' class="titreSerie"><div class ="rectangleSerie">' . $donnees['titre'] . '<br>' . $minia . '</div></a></br>';
+            if ($addSerie->rowCount() == 0) {
+                $seriesPref =  "<p>Vous n'avez pas de série préférée... Pour l'instant 😉</p>";
+            } else {
+                while ($donnees = $addSerie->fetch()) {
+                    $minia = '<img src="images/' . $donnees["img"] . '" height=200px width=500px>';
+                    $url = '?action=display-serie&serie=' . $donnees["id_serie"];
+                    $seriesPref .= '<a href=' . $url . ' class="titreSerie"><div class ="rectangleSerie">' . $donnees['titre'] . '<br>' . $minia . '</div></a></br>';
+                }
             }
         }
 
-        return '<div class = "container"><h3>Liste de vos séries préférées ⭐ :</h3> <p class="listeSerie">'.$seriesPref.'</p></div>';
+        return '<div class = "container"><h3>Liste de vos séries préférées ⭐ :</h3> <p class="listeSerie">' . $seriesPref . '</p></div>';
     }
 }
