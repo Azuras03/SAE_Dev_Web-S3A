0f689d2bb18b066cf9e4bdae5015fbb5245d0048
Azuras03 <nr03russo@gmail.com>
2022-11-10 10:49:09 +0100

    Refactoring the ActionDisplayFavoriteSeries.php to use the class Serie to render the list

--- src/classes/action/ActionDisplayFavoriteSeries.php
+++ src/classes/action/ActionDisplayFavoriteSeries.php
@@ -2,6 +2,7 @@
 
 namespace netvod\action;
 
+use netvod\catalogue\Serie;
 use netvod\db\ConnectionFactory;
 
 class ActionDisplayFavoriteSeries extends Action
@@ -12,18 +13,15 @@ class ActionDisplayFavoriteSeries extends Action
         $db = ConnectionFactory::makeConnection();
         $seriesPref = "";
 
-        $addSerie = $db->prepare("SELECT titre, img, id_serie FROM serie INNER JOIN user2serie ON user2serie.id_serie = serie.id INNER JOIN user ON user2serie.id_user = user.id WHERE user.id = ?;");
+        $addSerie = $db->prepare("SELECT titre, img, id_serie as id FROM serie INNER JOIN user2serie ON user2serie.id_serie = serie.id INNER JOIN user ON user2serie.id_user = user.id WHERE user.id = ?;");
         $addSerie->bindParam(1, unserialize($_SESSION['user'])->id);
         if ($addSerie->execute()) {
             if ($addSerie->rowCount() == 0) {
                 $seriesPref =  "<p>Vous n'avez pas de série préférée... Pour l'instant 😉</p>";
             } else {
                 $seriesPref .= '<p>Nombre de séries : ' . $addSerie->rowCount() . '</p>';
-                while ($donnees = $addSerie->fetch()) {
-                    $minia = '<img src="images/' . $donnees["img"] . '" height=200px width=500px>';
-                    $url = '?action=display-serie&serie=' . $donnees["id_serie"];
-                    $seriesPref .= '<a href=' . $url . ' class="titreSerie"><div class ="rectangleSerie">' . $donnees['titre'] . '<br>' . $minia . '</div></a></br>';
-                }
+
+                $seriesPref .= Serie::showSeriesTiles($addSerie);
             }
         }
 
